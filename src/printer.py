2bfc09b79554de48ac5e98bbdcfaeb6d41774b31
D1plo1d <d1plo1d@d1plo1d.com>
2013-04-20 03:18:01 -0400

    Adding a print thread. Work in progress

--- src/printer.py
+++ src/printer.py
@@ -1,4 +1,4 @@
-from threading import Thread
+from threading import Thread, RLock
 import re
 from better_serial import BetterSerial
 from event_emitter import EventEmitter
@@ -11,14 +11,29 @@ class Printer(EventEmitter):
     super(PrintCore, self)
     self.jobs = PrintJobQueue()
     self.settings = Settings()
+    self.verbose = False
+
+    # Serial
     self.serial = None
+    # Threads
+    self.read_thread = None
+    self.print_thread = None
+    # Locks
+    self._serial_write_lock = RLock()
+    self._resend_lock = RLock()
+    self._current_job_line_lock = RLock()
+    # Flags
     self._ready = False
+    self._printing = False
     self._disconnect = False
+    # Parsing Constants
     self._greetings = ('start','Grbl ', 'ok')
     self.sensor_names = {'t': 'extruder', 'b': 'bed'}
+
+    self.current_job = None
     self.sensors = {}
-    self._resend = None
-    self.verbose = False
+    self._resend_from = None
+    self._sent_lines = {}
 
   @save_args(port=None, baud=None)
   def connect(self, port, baud):
@@ -56,23 +71,54 @@ class Printer(EventEmitter):
     """
     Returns true if the printer is connected AND is ready receive gcodes.
     """
-    return self._ready
+    return self._ready and is_connected()
 
   def reset(self):
     if not self.is_connected(): return
     self.serial.setDTR(1)
     time.sleep(0.2)
     self.serial.setDTR(0)
-    self._resend = None
+    self._resend_lock.acquire()
+    self._resend_from = None
+    self._resend_lock.release()
 
-  def start(self, lines):
-    pass
+  def start(self):
+    self.print_thread = PrintThread(self)
+    self.print_thread.start()
 
   def pause(self):
-    pass
+    self._printing = False
+    self.print_thread.join()
+    self.fire("job_paused", self.current_job)
 
-  def send_now(self):
-    pass
+  def send_now(self, command):
+      """Sends a command to the printer ahead of the command queue, without a checksum
+      """
+      if self._ready:
+        self._send(command)
+      else:
+        print "Printer is not yet ready."
+
+  def _send(self, command, lineno = 0, calcchecksum = False):
+    if not self.is_connected():
+      print "Not connected to printer."
+      pass
+    if calcchecksum:
+      prefix = "N" + str(lineno) + " " + command
+      command = prefix + "*" + str(self._checksum(prefix))
+      if "M110" not in command: self._sent_lines[lineno] = command
+    # self.analyzer.Analyze(command) # run the command through the analyzer
+
+    if self.verbose:
+      print "SENT: ", command
+
+    self._serial_write_lock.acquire()
+    try:
+      self.serial.write(str(command+"\n"))
+    except SerialException, e:
+      print "Can't write to printer (disconnected?)."
+    self._serial_write_lock.release()
+
 
   def _run(self):
     """
@@ -149,9 +195,62 @@ class Printer(EventEmitter):
   def _parse_resend_line(self, words, line):
     # Teststrings for resend parsing       # Firmware     exp. result
     # line="rs N2 Expected checksum 67"    # Teacup       2
+    self._resend_lock.acquire()
     try:
-      self._resend = int( re.search('N:?\s?([0-9]+)', line).group(1) )
+      self._resend_from = int( re.search('N:?\s?([0-9]+)', line).group(1) )
     except:
       pass
+    self._resend_lock.release()
 
 
+PrintThread(thread):
+  __init__(self, printer):
+    self.p = printer
+
+  def _pop_next_job(self):
+    if len(self.p.job.list) == 0: return False
+    self.p.current_job = self.p.job.list.left_pop()
+    self.fire("job_started", self.current_job)
+    return True
+
+
+  def run(self):
+    while self.p._printing and self.p.is_ready():
+      # when each job finishes pop the next job off the queue.
+      # when all jobs are finished, end the thread.
+      if self.p.current_job == None
+        self.sent_lines = {}
+        self.line_number = 0
+        self.p.current_job_line = 0
+        self._send("M110", -1, True)
+        if not self._pop_next_job(): return
+
+      self._send_next_line()
+
+  def _send_next_line(self):
+    # Deal with any lines that need to be resent due to comms issues
+    self.p._resend_lock.aquire()
+    if self.p._resend_from != None and self.p._resend_from < self.line_number:
+      self.p._send(self.sent_lines[self._resend_from], self.p._resend_from, False)
+      self.p._resend_from += 1
+      self.p._resend_lock.release()
+      return
+    self.p.resend_from = None
+    self.p._resend_lock.release()
+
+    # If there are no lines needing to be resend then continue with the print
+    if self.p.current_job_line < len(self.p.current_job):
+        tline = self.p.current_job[self.p.current_job_line]
+        tline = tline.split(";")[0]
+
+        if len(tline) > 0:
+            self.p._send(tline, self.line_number, True)
+            self.line_number += 1
+        self.p._current_job_line_lock.acquire()
+        self.p.current_job_line += 1
+        self.p._current_job_line_lock.release()
+
+    # once the job is complete fire an event
+    else:
+      self.p.fire("job_finished", self.p.current_job)
+      self.p.current_job = None
